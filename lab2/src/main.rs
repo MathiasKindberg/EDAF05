f53f326975bd320159480c82baf087d548a2299a
Mathias Kindberg <mathias.kindberg@gmail.com>
2020-04-06 19:10:58 +0200

    Finished lab2

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,15 +1,12 @@
 use std::io::{self, Read};
 // Get-Content -Path .\1.in -Raw | cargo run
 
-
-// Either one big vector or use hashmap for storage.
-//  https://doc.rust-lang.org/std/collections/struct.HashMap.html
+#[derive(Debug, Clone)]
 struct Word {
     word: String,
     index: usize,
-    connections: Vec<usize>,
+    edges: Vec<usize>,
 }
-
 
 fn get_input() -> Result<Vec<String>, std::io::Error> {
     let mut raw_input = String::new();
@@ -19,18 +16,21 @@ fn get_input() -> Result<Vec<String>, std::io::Error> {
     let mut clean_input: Vec<String> = raw_input
         .split(&[' ', '\n'][..]) // Split the input by line and/or space
         .map(|s| s.to_string()) // Convert the &str to string.
-        .collect();             // Collect into vector.
+        .collect(); // Collect into vector.
 
     clean_input.retain(|s| s != ""); // Remove empty elements.
     Ok(clean_input)
 }
 
-fn check_alike(word1: &String, word2: &String) -> bool {
+fn check_alike(word1: &str, word2: &str) -> bool {
     let mut used_chars = vec![0; 5]; // Keep track of which chars we have already matched on.
 
+    // .chars() iterates over the string using Unicode Scalar Values, which works for English and similar
+    // languages that only contains one scalar value per character. This would, as an example not, work on
+    // emojis or asian characters which requires iteration and slicing over grapheme clusters instead.
     for ch1 in word1[1..].chars() {
-        for (i, ch) in word2.chars().enumerate() {
-            if ch == ch1 && used_chars[i] == 0 {
+        for (i, ch2) in word2.chars().enumerate() {
+            if ch1 == ch2 && used_chars[i] == 0 {
                 used_chars[i] = 1;
                 break; // Don't match twice on the same character.
             }
@@ -39,46 +39,101 @@ fn check_alike(word1: &String, word2: &String) -> bool {
     used_chars.iter().sum::<u32>() == 4
 }
 
+fn get_index(graph: &[Word], word: &str) -> Option<usize> {
+    for v in graph {
+        if v.word == word {
+            return Some(v.index);
+        }
+    }
+    None
+}
+
+fn bfs(graph: &[Word], start: usize, terminate: usize) -> String {
+    // Initialize values we use while working the graph. Better here than
+    // storing something unrelated to the graph in the node since these are
+    // based on the search and not the actual structure.
+
+    let mut visited: Vec<bool> = vec![false; graph.len()];
+    visited[start] = true;
+
+    let mut pred: Vec<Option<usize>> = vec![None; graph.len()];
+
+    let mut q: Vec<usize> = Vec::new();
+    q.push(start);
+
+    if start == terminate {
+        return "0".to_string();
+    }
+
+    while !q.is_empty() {
+        let v = q.remove(0);
+
+        for w in &graph[v].edges.clone() {
+            if !visited[*w] {
+                // * dereferences the reference.
+
+                visited[*w] = true;
+                pred[*w] = Some(v);
+
+                q.push(*w);
+
+                if *w == terminate {
+                    let mut nbr_edges = 0;
+                    let mut index = *w;
+
+                    // Instead of the linked list like approach, we step through the array based
+                    // on the index. I.e. doing the same but calling the index our pointer.
+                    while index != start {
+                        index = pred[index].unwrap();
+                        nbr_edges += 1;
+                    }
+                    return (nbr_edges).to_string();
+                }
+            }
+        }
+    }
+    "Impossible".to_string()
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = get_input()?;
-    let num_words:usize = input[0].parse()?;
-    let num_connections:usize = input[1].parse()?;
+    let num_words: usize = input[0].parse()?;
+    //let num_queries: usize = input[1].parse()?;
 
-    println!("there retch: {}", check_alike(&"there".to_string(), &"retch".to_string()));
-    println!("where there: {}", check_alike(&"where".to_string(), &"there".to_string()));
-    println!("hello lolem: {}", check_alike(&"hello".to_string(), &"lolem".to_string()));
-    println!("lolem hello: {}", check_alike(&"lolem".to_string(), &"hello".to_string()));
+    // We store everything in a big vector which we index into since Rust is extremely picky about
+    // cyclical data structures due to it's concurrency and memory correctness guarantees.
+    let mut graph: Vec<Word> = Vec::new();
 
-    println!("{:?}", num_words);
-    println!("{:?}", num_connections);
-
-    let mut words = Vec::new();
-
-    for word in input[2..num_words+2].iter() {
-        println!("{}", word);
-        for alike in &words {
-            if check_alike(word, alike) {
-                // Words alike, do magic.
+    // Index words and find edges.
+    for (index, word) in input[2..num_words + 2].iter().enumerate() {
+        let mut edges = Vec::new();
+        for alike in &mut graph {
+            if check_alike(word, &alike.word) {
+                edges.push(alike.index);
             }
-            if check_alike(alike, word) {
-                // Words alike, do magic
+            // Checking backwards allows us to both index and build the graph in one go.
+            if check_alike(&alike.word, word) {
+                alike.edges.push(index);
             }
-
-            // Remember, we need to check both ways, since some words have double connections
-            // and others dont.
-            println!("{} {}: {}", word, alike, check_alike(word, alike));
-            println!("{} {}: {}", alike, word, check_alike(alike, word));
-
         }
-        words.push(word.to_string());
+        graph.push(Word {
+            word: word.to_string(),
+            index,
+            edges,
+        });
     }
 
-    // Process connection queries in chunks of 2.
-    for connection in input[num_words + 2..].chunks(2) {
-        println!("{:?}", connection);
-    }
+    let mut output = String::new();
 
+    // Process queries in chunks of 2.
+    for query in input[num_words + 2..].chunks(2) {
+        let start = get_index(&graph, &query[0]).expect("Word not found");
+        let terminate = get_index(&graph, &query[1]).expect("Word not found");
+        //println!("{}",bfs(&mut graph, start, terminate));
+        output.push_str(&bfs(&graph, start, terminate));
+        output.push('\n');
+    }
+    print!("{}", output);
 
     Ok(())
 }
-
